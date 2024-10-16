f6448471b6392f86f45c2533b15902f31caef41d
arunastanley <arunapaulus@gmail.com>
2024-10-16 17:05:34 -0500

    Committing validation-errors branch

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,17 +1,31 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 4,max = 30,message = "Name can be 4-30 characters long")
     private String name;
+
+    @Size(max = 300,message = "Description cannot be longer than 300 characters")
     private String desc;
 
-    public Event(String name,String desc) {
+    @NotBlank
+    @Email(message = "Enter a valid Email id")
+    private String email;
+
+    public Event(String name,String desc,String email) {
         this.name = name;
         this.desc = desc;
+        this.email = email;
         this.id = nextId;
         nextId++;
     }
@@ -30,6 +44,14 @@ public class Event {
 
     public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public int getId() {
