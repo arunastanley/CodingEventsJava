50e172c212d200dfe78714f9e1e8776b141694c2
arunastanley <arunapaulus@gmail.com>
2024-10-28 09:46:27 -0500

    Committing db-config branch

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -14,6 +14,7 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
